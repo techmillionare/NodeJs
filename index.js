330319bec24bf48eb4c706a250ca184bd98f6d2f
techmillionare <dayanandmishra987@gmail.com>
2024-07-13 21:38:50 +0530

    Node Learning day1-3

---
+++ index.js
@@ -0,0 +1,43 @@
+   /* DAY 1 */
+// const app = require('./app');
+
+// // var a = 20;
+// // var b = 54;
+// // var c = 90;
+
+// // console.log(a+b+c);
+
+// console.log(app.z());
+
+// const arr = [1,2,3,4,5];
+// let res = arr.filter((items) => {
+//    return items < 3;
+// })
+
+// console.log(res);
+
+
+        /*DAY 2 */   // core module - 1. global module 2.non-global module
+// const fs = require('fs');
+//console.log("Dayanand Mishra");
+//fs.writeFileSync("hello.txt","xyz");
+// console.log("->>",__filename);
+// console.log("->>",__dirname);
+                /*DAY 3 */
+// const fs = require('fs').writeFileSync;
+// fs("code.txt","Learning Nodejs");
+
+     /*HTTP->non global core module
+     */  //it handles the request and reponse in nodejs
+// const http = require('http');
+
+// http.createServer((req,resp)=>{
+//         resp.write("<h1>Dayanand Mishra , Learning NodeJs</h1>")
+//         resp.end();
+// }).listen(4500); //createServer takes function as a parameter
+
+        /*Package.json*/
+ // it keeps all the information/details about our code
+ // e.g. bulid command,package info,etc 
+ // step 1 - npm init
+ 
